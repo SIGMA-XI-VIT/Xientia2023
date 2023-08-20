378e6b63e1c4ff539218125f06ff470170663a7c
crocs-in-socks <gouriswag1@gmail.com>
2023-08-20 20:11:37 +0530

    Added submission route

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -2,11 +2,14 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./keys");
 
-const dummyUser = {
-    id: "qwerty69420",
-    name: "Dummy User",
-    email: "dummy@example.com"
-};
+// TEMP
+const User = require("../TEMP/user-model")
+
+// const dummyUser = {
+//     id: "qwerty69420",
+//     name: "Dummy User",
+//     email: "dummy@example.com"
+// };
 
 passport.serializeUser((user, done) => {
     console.log("Serialized user.");
@@ -14,8 +17,14 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    console.log("Deserialized user.");
-    done(null, dummyUser);
+    // console.log("Deserialized user.");
+    
+    // TEMP
+    User.findById(id).then((user) => {
+        done(null, user)
+    })
+
+    // done(null, dummyUser);
 });
 
 passport.use(
@@ -27,11 +36,29 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         // Passport callback function
         console.log(profile);
+
         // TODO: Save user data to database if needed
         // Replace the following line with database logic
         // Retrieve user data from database using profile.id
-
-        done(null, dummyUser);
+        
+        // TEMP
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser)
+            {
+                console.log("User is: ", currentUser)
+                done(null, currentUser)
+            }
+            else
+            {
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log(`New user created: ${newUser}`)
+                    done(null, newUser)
+                })
+            }
+        })
     })
 );
 
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,24 @@ const passport = require("passport")
 
 const auth_routes = require("./routes/auth-routes")
 const countdown_routes = require("./routes/countdown-routes")
+const submission_routes = require("./routes/submission-routes")
 
 const passport_setup = require("./config/passport-setup")
 const keys = require("./config/keys")
 
 const app = express()
 const port = 5000
+
+// TEMPORARY MONGODB CONNECTION
+const mongoose = require("mongoose")
+mongoose.connect(keys.mongodb.dbURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(error => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
 
 app.use(cookie_session({
     maxAge: 24 * 60 * 60 * 1000,
@@ -21,9 +33,15 @@ app.use(cookie_session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// graphql setup
+// app.use("/graphql", graphqlHTTP({
+
+// }))
+
 // Set up routes
 app.use("/auth", auth_routes)
 app.use("/countdown", countdown_routes)
+app.use("/submission", submission_routes)
 
 app.get("/", (req, res) => {
     res.send("Hello mic check 1, 2, 3, it is working.")
